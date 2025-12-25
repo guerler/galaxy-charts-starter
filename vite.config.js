065395ec43be171a19bc052d4dbe9ef0e4f1a02f
guerler <aysam.guerler@gmail.com>
2025-12-25 15:11:00 +0300

    Update to latest dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 
 import { viteConfigCharts } from "./vite.config.charts";
@@ -9,8 +9,8 @@ export default defineConfig({
     ...viteConfigCharts,
     plugins: [vue(), tailwindcss()],
     test: {
+        environment: "happy-dom",
         globals: true,
-        environment: "jsdom",
         include: ["src/**/*.test.{js,ts,jsx,tsx}"],
     },
 });
